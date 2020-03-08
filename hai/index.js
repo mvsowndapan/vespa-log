804bd84cfb51eb8c370fceb9bbf1124f42b4cc41
mvsowndapan <mvsomupushpa@gmail.com>
2020-03-08 14:49:08 +0530

    exported

---
+++ hai/index.js
@@ -0,0 +1,3 @@
+const vespa = require('vespa-log');
+
+vespa.log('hsai');
---
+++ hai/node_modules/vespa-log/index.js
@@ -0,0 +1,9 @@
+const vespa = {
+	log: (...arguments) => console.log(...arguments),
+	info: (...arguments) => console.info(...arguments),
+	error: (...arguments) => console.error(...arguments)
+};
+
+vespa.log('fuber');
+vespa.error('jskjn0');
+vespa.info('wfhrb');
